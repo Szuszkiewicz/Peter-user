fe1b76adc9e362ec5210fd1daa60d8905ecb2126
Szuszkiewicz <yck920601@gmail.com>
2026-05-31 21:38:53 +0800

    同意用户协议才能注册 加入前端，但是还没有集成到网关

--- peter-user-main/src/main/java/com/Peter/common/CorsConfig.java
+++ peter-user-main/src/main/java/com/Peter/common/CorsConfig.java
@@ -10,10 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
     private CorsConfiguration buildConfig(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+        /**
+         *  addAllowedOriginPatterns("*")
+         *  可以与 allowCredentials(true) 同时使用
+         *  支持更复杂的模式匹配（如 https://*.example.com)
+         */
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowCredentials(true);//跨域请求中 允许cookie http认证 客户端ssl证书 其他请求头
         // 跨域请求有效时长 1天
         corsConfiguration.setMaxAge(24 * 60 * 60L);
 
